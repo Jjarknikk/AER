7cf9ddf78c96ff33a3fce7d0328bd002e75e8294
Jjarknikk <skellington106@hotmail.com>
2026-08-24 19:59:25 +0100

    Add AERMac executable target

--- Package.swift
+++ Package.swift
@@ -8,11 +8,13 @@ let package = Package(
     ],
     products: [
         .library(name: "AERCore", targets: ["AERCore"]),
-        .executable(name: "aer-sim", targets: ["aer-sim"])
+        .executable(name: "aer-sim", targets: ["aer-sim"]),
+        .executable(name: "AERMac", targets: ["AERMac"])
     ],
     targets: [
         .target(name: "AERCore"),
         .executableTarget(name: "aer-sim", dependencies: ["AERCore"]),
+        .executableTarget(name: "AERMac", dependencies: ["AERCore"]),
         .testTarget(name: "AERCoreTests", dependencies: ["AERCore"])
     ]
 )
